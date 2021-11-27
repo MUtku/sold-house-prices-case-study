e678ae88d89a52717284950c43e147709ff9b0b8
Melih Utku Şahin <mutku@Melihs-MacBook-Pro.local>
2021-11-27 15:04:08 +0300

    Add class models

--- soldhouseprices/settings.py
+++ soldhouseprices/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'soldhouseprices_app',
     'whitenoise.runserver_nostatic',
 ]
 
--- soldhouseprices_app/models.py
+++ soldhouseprices_app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Houses(models.Model):
+    id = models.CharField(max_length=60, primary_key=True)
+    price = models.IntegerField()
+    date = models.DateField()
+    zipcode = models.CharField(max_length=10)
+    property_type = models.CharField(max_length=1)
+    
