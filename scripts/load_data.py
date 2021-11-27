59e11b2a051297f4316a2ef4554e189515c6fe4b
Melih Utku Şahin <mutku@Melihs-MacBook-Pro.local>
2021-11-27 15:55:31 +0300

    update dataloader

--- scripts/load_data.py
+++ scripts/load_data.py
@@ -3,19 +3,17 @@ import os
 from soldhouseprices_app.models import house_transactions
 
 def run():
-    file  = open("./");
-    readed_file = csv.reader(file);
+    file  = open("./pp-2021_clean.csv");
+    file_read = csv.reader(file);
     
     house_transactions.objects.all().delete()
 
-    for record in readed_file:
+    for record in file_read:
         print(record)
         id=record[0][1:-1]
-        print(id)
-        break
         price = record[1]
-        transaction_date = record[2]
-        zip_code = record[3].split(" ")[0]
+        transaction_date = record[2].split(' ')[0]
+        zip_code = record[3].split(' ')[0]
         property_type = record[4]
 
         house_transactions.objects.create(id =id, price = price, date = transaction_date, zip = zip_code, property_type = property_type)
