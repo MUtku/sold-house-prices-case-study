e8756fe55221b2c362907d4b9a7ac8a96fb12b41
Melih Utku Şahin <mutku@Melihs-MBP.home>
2021-12-02 03:15:17 +0300

    Modify averagehouseprices calculation to match needs

--- soldhouseprices_app/views.py
+++ soldhouseprices_app/views.py
@@ -1,7 +1,7 @@
 from django.db.models.functions.datetime import TruncYear
 from django.http.response import JsonResponse
 from django.db.models.aggregates import Avg
-from django.db.models.functions import TruncDate, TruncMonth, TruncYear
+from django.db.models.functions import TruncMonth
 from datetime import datetime
 from rest_framework.decorators import api_view
 from soldhouseprices_app.models import house_transactions
@@ -53,10 +53,25 @@ def averagehouseprices(request):
 
         detached_result_set = house_transactions.objects.filter(zipcode = zipcode_value,
             date__range = [from_date_value, to_date_value], property_type = 'D')\
-                .annotate(dates = TruncDate('date')).values('dates')\
-                        .annotate(average_price = Avg('price')).values('dates', 'average_price')
+                .annotate(month = TruncMonth('date')).values('month')\
+                        .annotate(average_price = Avg('price')).values('month', 'average_price')
 
-        print(detached_result_set)
+        semi_detached_result_set = house_transactions.objects.filter(zipcode = zipcode_value,
+            date__range = [from_date_value, to_date_value], property_type = 'S')\
+                .annotate(month = TruncMonth('date')).values('month')\
+                        .annotate(average_price = Avg('price')).values('month', 'average_price')
+        terraced_result_set = house_transactions.objects.filter(zipcode = zipcode_value,
+            date__range = [from_date_value, to_date_value], property_type = 'T')\
+                .annotate(month = TruncMonth('date')).values('month')\
+                        .annotate(average_price = Avg('price')).values('month', 'average_price')
+        flats_result_set = house_transactions.objects.filter(zipcode = zipcode_value,
+            date__range = [from_date_value, to_date_value], property_type = 'F')\
+                .annotate(month = TruncMonth('date')).values('month')\
+                        .annotate(average_price = Avg('price')).values('month', 'average_price')
+        result_obj = {'detached': detached_result_set.tolist(), 
+                        'semi_detached': semi_detached_result_set.tolist(),
+                        'terraced': terraced_result_set.tolist(),
+                        'flats': flats_result_set.tolist()}
     except Exception as e:
         print(e)
     return JsonResponse(result_obj)
